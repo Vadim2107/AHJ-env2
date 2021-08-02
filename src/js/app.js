cc8e6926ae87e811b6df72ee7744a1cc9aa780e6
Vadim2107 <58651870+Vadim2107@users.noreply.github.com>
2021-08-02 21:21:31 +0300

    edit app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,4 @@
+/* eslint-disable no-console */
 // TODO: write code here
 
-// comment this to pass build
-// const unusedVariable = 'variable';
-
-// for demonstration purpose only
-// export default function demo(value) {
-//   return value;
-// }
-
-// console.log('app.js included');
+console.log('app.js included');
